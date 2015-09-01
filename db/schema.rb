1742f9c4fe4bb646d6b59bdeaeb4d416113bfa2a
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-09-01 10:46:28 +0200

    Create the ability to encrypt user password #4

--- db/schema.rb
+++ db/schema.rb
@@ -1,13 +1,13 @@
 # encoding: UTF-8
-ActiveRecord::Schema.define(version: 20150831224902) do
+ActiveRecord::Schema.define(version: 20150901081947) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-
 end
