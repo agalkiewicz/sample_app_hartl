04d187d0b3c9f10d371dc26f171044ee0e67f4be
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-10-04 20:40:40 +0200

    Improve users_login_test (ex. 8.2)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params.fetch(:session)[:email].downcase)
-    if user && user.authenticate(params.fetch(:session)[:password])
-      log_in user
-      params.fetch(:session)[:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+    @user = User.find_by(email: params.fetch(:session)[:email].downcase)
+    if @user && @user.authenticate(params.fetch(:session)[:password])
+      log_in @user
+      params.fetch(:session)[:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
