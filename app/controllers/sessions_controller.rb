dedbe3e4f383b2a12645b767cc95c0d39141ac66
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-10-08 18:22:40 +0200

    Friendly forwarding #9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params.fetch(:session)[:password])
       log_in user
       params.fetch(:session)[:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
