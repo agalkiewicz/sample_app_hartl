7033cf5761789f9afd0b4ff5737db6898d470be2
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-10-15 22:13:22 +0200

    Add admin attribute to User model #9

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 20150929193940) do
+ActiveRecord::Schema.define(version: 20151015183653) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -7,6 +7,7 @@ ActiveRecord::Schema.define(version: 20150929193940) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
