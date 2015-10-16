ef6e1e2e112a3e6e6ae0bcb19a56c643d70dd04a
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-10-16 21:58:24 +0200

    Correct syntax #9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,11 +63,11 @@ class UsersController < ApplicationController
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to root_url unless current_user?(@user)
     end
 
     # Confirms an admin user.
     def admin_user
-      redirect_to(root_url) unless current_user.admin?
+      redirect_to root_url unless current_user.admin?
     end
 end
