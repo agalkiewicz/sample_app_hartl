bd177eca770c4fd05255570da95c8527bccf71de
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-11-01 12:46:55 +0100

    Add account activations resource #11

--- db/schema.rb
+++ db/schema.rb
@@ -1,13 +1,16 @@
-ActiveRecord::Schema.define(version: 20151015183653) do
+ActiveRecord::Schema.define(version: 20151101101142) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
