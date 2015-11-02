458be636807b05b04c2bd50670d3fa72bc6f4a06
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-11-02 09:44:36 +0100

    Add account activation mailer method #11

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,10 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -30,6 +24,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
