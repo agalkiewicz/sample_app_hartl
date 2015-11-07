b2c93dbabc418ca36c64362b8f9702d55c7e4288
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-11-07 12:14:00 +0100

    Back-end for user account activation #11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params.fetch(:session)[:email].downcase)
     if user && user.authenticate(params.fetch(:session)[:password])
-      log_in user
-      params.fetch(:session)[:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params.fetch(:session)[:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = "Invalid email/password combination."
       render 'new'
     end
   end
