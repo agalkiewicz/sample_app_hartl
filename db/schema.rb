cc527c3349c056b2b94062b2866f53d1d2001bc6
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-11-20 11:29:19 +0100

    Back-end for password reset functionality #11

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 20151101101142) do
+ActiveRecord::Schema.define(version: 20151119174757) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -11,6 +11,8 @@ ActiveRecord::Schema.define(version: 20151101101142) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
