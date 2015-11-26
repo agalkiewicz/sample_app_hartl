2dddec0a3f38caa264b49ae3f7fb4f824486ffeb
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-11-26 23:57:17 +0100

    Add Micropost model #13

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,14 @@
-ActiveRecord::Schema.define(version: 20151119174757) do
+ActiveRecord::Schema.define(version: 20151126224346) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -9,7 +19,7 @@ ActiveRecord::Schema.define(version: 20151119174757) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.boolean  "activated",         default: false, null: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
