925064ddba675ba0be7d22c7c242d63336b0cb6a
agalkiewicz <alicja.galkiewicz@gmail.com>
2015-12-05 14:18:37 +0100

    User can upload images #13

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,11 @@
-ActiveRecord::Schema.define(version: 20151126224346) do
+ActiveRecord::Schema.define(version: 20151205123306) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
