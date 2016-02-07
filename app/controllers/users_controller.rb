92d317a7788e2f84f4bb0e4c9f4d73c2dde31689
agalkiewicz <alicja.galkiewicz@gmail.com>
2016-02-07 18:40:08 +0100

    Make some corrections #15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: %i(index edit update destroy following followers)
+  before_action :correct_user,   only: %i(edit update)
   before_action :admin_user,     only: :destroy
   
   def index
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
     @title = "Followers"
     @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
+    render "show_follow"
   end
 
   private
